8d0c939af1f10fe6b3c6eefafad777b47d73efaf
Oraclee5321 <98158236+Oraclee5321@users.noreply.github.com>
2023-01-12 14:52:05 +0000

    Update account-settings.php

--- account-settings.php
+++ account-settings.php
@@ -48,6 +48,28 @@ $user = new User($_SESSION['UID'],$_SESSION['username'],$_SESSION['email'],$_SES
                     </form>
                 </div>
             </div>
+        </div><!-- Edit Username Modal -->
+        <div class="modal fade" id="editEmailModal" tabindex="-1" aria-labelledby="editEmailLabel" aria-hidden="false"><!-- Edit Email Modal -->
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="editEmailLabel">Edit Username</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="account-settings.php" method="POST">
+                        <div class="modal-body">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" maxlength="50" minlength="1" id="editEmailInput" name="editEmailInput">
+                                <span class="input-group-text" id="editEmailCharCounter"> / 50</span>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <input type="submit" class="btn btn-primary" value="Edit" name="editEmailButton" data-bs-dismiss="modal">
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
         <div class="container ">
             <div class="row">
@@ -77,9 +99,8 @@ $user = new User($_SESSION['UID'],$_SESSION['username'],$_SESSION['email'],$_SES
                     <h2 class="text-start"><?php echo $user->email ?></h2>
                 </div>
                 <div class="col-6 col-md-4">
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editEmailModal">
-                        <h3 class="text-start">Edit</h3>
-                    </button>
+                    <h2><input type="button" class="btn btn-primary" value="Change" onclick="currentEmail()" /></h2>
+                    <button type="button" style="display:none" data-bs-toggle="modal" data-bs-target="#editEmailModal") id="editEmailButton"></button>
                 </div>
             </div>
             <br>
@@ -102,6 +123,15 @@ $user = new User($_SESSION['UID'],$_SESSION['username'],$_SESSION['email'],$_SES
                     $('#editUsernameCharCounter').text(<?php echo strlen($user->username) ?> + " / 50");
                 });
                 document.getElementById("editUsernameButton").click();
+            }
+            function currentEmail(){
+                var modal = document.getElementById("editEmailModal");
+                modal.addEventListener('show.bs.modal',function(event){
+                    var email = <?php echo $user->email ?>;
+                    document.getElementById("editEmailInput").value = email;
+                    $('#editUsernameCharCounter').text(<?php echo strlen($user->email) ?> + " / 50");
+                });
+                document.getElementById("editEmailButton").click();
             }
         </script>
     </body>
